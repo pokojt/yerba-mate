ebbaf4eb94d42b813a16d3450fdc5988cbc23db1
Taylor Pokoj <pokojt@gmail.com>
2016-04-06 13:11:57 -0700

    Add funtionality to edit product on admin-category page

--- app/components/edit-product.js
+++ app/components/edit-product.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editFormShow: false,
+  actions: {
+    toggleEdit() {
+      this.toggleProperty('editFormShow');
+    },
+    editProduct(product) {
+      var editFormInputs = {
+        name: this.get('name'),
+        seller: this.get('seller'),
+        category: this.get('category'),
+        image: this.get('image'),
+        description: this.get('description'),
+        price: this.get('price'),
+        reviews: this.get('reviews'),
+    };
+      this.toggleProperty('editFormShow');
+      this.sendAction('editProduct', product, editFormInputs);
+    }
+  }
 });
