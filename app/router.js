f0686232a38b45658a351cab427bc96f8f3ed465
Taylor Pokoj <pokojt@gmail.com>
2016-04-06 13:57:50 -0700

    Add dynamic routing to each product for a details page

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
   this.route('cart');
   this.route('category', {path: '/category/:category_id'});
   this.route('admin-category', {path: '/admin-category/:category_id'});
+  this.route('product-details', {path: '/product-details/:product_id'});
 });
 
 export default Router;
