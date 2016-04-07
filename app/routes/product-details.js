4bc89fc99b02ce8e9f656237b76fbcb1ef2741cd
Taylor Pokoj <pokojt@gmail.com>
2016-04-07 15:55:20 -0700

    hot fix

--- app/routes/product-details.js
+++ app/routes/product-details.js
@@ -2,24 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   shoppingCart: Ember.inject.service(),
-  // myFavorites: Ember.inject.service(),
   addedToCart: false,
   model(params) {
     return this.store.findRecord('product', params.product_id);
   },
   actions: {
     addToCart(product) {
+      // debugger;
       alert("Added!");
-      this.get('shoppingCart').add(product);
       this.set('addedToCart', true);
+      this.get('shoppingCart').add(product);
     },
     removeFromCart(product) {
       this.set('addedToCart', false);
       this.get('shoppingCart').remove(product);
     },
-    // addToFavorites(favorite) {
-    //   this.get('myFavorites').add(favorite);
-    // },
     saveReview(formInputs) {
       var newReview = this.store.createRecord('review', formInputs);
       var product = formInputs.product;
